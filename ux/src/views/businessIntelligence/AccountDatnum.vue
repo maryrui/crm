177eaacde270adf1c8d93e7a84cfd34e45c4ce10
maryrui <solo_days@163.com>
2019-10-17 23:39:54 +0800

    修改账款分析数据

--- ux/src/views/businessIntelligence/AccountDatnum.vue
+++ ux/src/views/businessIntelligence/AccountDatnum.vue
@@ -16,7 +16,9 @@
                     导出
                 </el-button>
                 <el-table :data="list"
-                          height="800"
+                          max-height="800"
+                          :show-summary="true"
+                          :summary-method="summarySpanMethod"
                           stripe
                           border
                           :span-method="objectSpanMethod"
@@ -47,7 +49,12 @@
             return {
                 rowSpanData: [],
                 spanArr: [],
+                codeArr: [],
+                code: [],
                 pos: [],
+                sum: 0,
+                codeSum: 0,
+                detSum: 0,
                 downloadLoading: false,
                 fieldList: [
                     { field: 'contract_id', name: '订单编号' },
@@ -57,9 +64,10 @@
                     { field: 'money', name: '订单金额' },
                     { field: 'invoice_code', name: '发票编号' },
                     { field: 'invoice_money', name: '发票金额' },
-                    { field: 'return_date', name: '回款日期' },
+                    { field: 'debt', name: '欠款金额' },
+                    { field: 'return_num', name: '回款编号' },
                     { field: 'return_money', name: '回款金额' },
-                    { field: 'debt', name: '欠款金额' }
+                    { field: 'return_date', name: '回款日期' }
                 ]
             }
         },
@@ -83,17 +91,69 @@
                     if (i === 0) {
                         this.spanArr.push(1)
                         this.pos = 0
+                        this.codeArr.push(1)
+                        this.code = 0
                     } else {
                         // 判断当前元素与上一个元素是否相同
                         if (data[i].contract_id === data[i - 1].contract_id) {
                             this.spanArr[this.pos] += 1
                             this.spanArr.push(0)
+                            if (data[i].invoice_code === data[i - 1].invoice_code) {
+                                this.codeArr[this.code] += 1
+                                this.codeArr.push(0)
+                            } else {
+                                this.codeArr.push(1)
+                                this.code = i
+                            }
                         } else {
                             this.spanArr.push(1)
                             this.pos = i
+                            this.codeArr.push(1)
+                            this.code = i
                         }
                     }
                 }
+            },
+            summarySpanMethod(params) {
+                const { columns, data } = params
+                const sums = []
+                columns.forEach((column, index) => {
+                    if (index === 0) {
+                        sums[index] = '总价'
+                        return
+                    }
+                    if (index === 1 || index === 2 || index === 3 || index === 5 || index === 8 || index === 10) {
+                        sums[index] = ''
+                        return
+                    }
+                    if (index === 4) {
+                        sums[index] = this.sum + '元'
+                        return
+                    }
+                    if (index === 6) {
+                        sums[index] = this.codeSum + '元'
+                        return
+                    }
+                    if (index === 7) {
+                        sums[index] = this.detSum + '元'
+                        return
+                    }
+                    const values = data.map(item => Number(item[column.property]))
+                    if (!values.every(value => isNaN(value))) {
+                        sums[index] = values.reduce((prev, curr) => {
+                            const value = Number(curr)
+                            if (!isNaN(value)) {
+                                return prev + curr
+                            } else {
+                                return prev
+                            }
+                        }, 0)
+                        sums[index] += ' 元'
+                    } else {
+                        sums[index] = 'N/A'
+                    }
+                })
+                return sums
             },
             objectSpanMethod({ row, column, rowIndex, columnIndex }) {
                 if (columnIndex === 0 || columnIndex === 1 || columnIndex === 2 || columnIndex === 3 || columnIndex === 4) {
@@ -104,24 +164,57 @@
                         colspan: _col
                     }
                 }
+                if (columnIndex === 5 || columnIndex === 6 || columnIndex === 7) {
+                    const _row1 = this.codeArr[rowIndex]
+                    const _col1 = _row1 > 0 ? 1 : 0
+                    return {
+                        rowspan: _row1,
+                        colspan: _col1
+                    }
+                }
             },
             processData(list) {
                 var arr = []
+                this.sum = 0
+                this.codeSum = 0
+                this.detSum = 0
                 for (var i = 0; i < list.length; i++) {
+                    this.sum += parseFloat(list[i].money)
                     if (list[i].receivables_plan.length > 0) {
                         for (var j = 0; j < list[i].receivables_plan.length; j++) {
-                            arr.push({
-                                contract_id: list[i].num,
-                                customer_name: list[i].customer_name,
-                                realname: list[i].realname,
-                                money: list[i].money,
-                                name: list[i].name,
-                                return_money: list[i].receivables_plan[j].receivables_money,
-                                invoice_money: list[i].receivables_plan[j].money,
-                                invoice_code: list[i].receivables_plan[j].invoice_code,
-                                return_date: list[i].receivables_plan[j].return_date,
-                                debt: list[i].receivables_plan[j].balance
-                            })
+                            this.codeSum += parseFloat(list[i].receivables_plan[j].money)
+                            this.detSum += parseFloat(list[i].receivables_plan[j].balance)
+                            if (list[i].receivables_plan[j].receivables.length > 0) {
+                                for (var m = 0; m < list[i].receivables_plan[j].receivables.length; m++) {
+                                    arr.push({
+                                        contract_id: list[i].num,
+                                        customer_name: list[i].customer_name,
+                                        realname: list[i].realname,
+                                        money: list[i].money,
+                                        name: list[i].name,
+                                        return_money: list[i].receivables_plan[j].receivables[m].money,
+                                        invoice_money: list[i].receivables_plan[j].money,
+                                        invoice_code: list[i].receivables_plan[j].invoice_code,
+                                        return_date: list[i].receivables_plan[j].receivables[m].return_time,
+                                        return_num: list[i].receivables_plan[j].receivables[m].number,
+                                        debt: list[i].receivables_plan[j].balance
+                                    })
+                                }
+                            } else {
+                                arr.push({
+                                    contract_id: list[i].num,
+                                    customer_name: list[i].customer_name,
+                                    realname: list[i].realname,
+                                    money: list[i].money,
+                                    name: list[i].name,
+                                    return_money: 0,
+                                    invoice_money: list[i].receivables_plan[j].money,
+                                    invoice_code: list[i].receivables_plan[j].invoice_code,
+                                    debt: list[i].receivables_plan[j].balance,
+                                    return_date: 0,
+                                    return_num: 0
+                                })
+                            }
                         }
                     } else {
                         arr.push({
@@ -133,6 +226,7 @@
                             return_money: 0,
                             invoice_code: 0,
                             return_date: 0,
+                            return_num: 0,
                             invoice_money: 0,
                             debt: 0
                         })
@@ -141,6 +235,8 @@
                 this.list =  arr
                 this.spanArr = []
                 this.pos = []
+                this.codeArr = []
+                this.code = []
                 this.getSpanArr(arr)
             }
         }
